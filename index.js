5c6c37e6c174b2a0e337b85cc211944a65145ab0
david parloir <david.parloir@gmail.com>
2022-04-21 13:13:19 +0200

    Switch case indentation
    The default is zero which forces the `case`s to be on the same indent as the `switch`, we want them to be 2 spaces furtther

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ module.exports = {
         'avoidEscape': true,
       },
     ],
-    'indent': ['error', 2],
+    'indent': ['error', 2, {
+      SwitchCase: 1
+    }],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
